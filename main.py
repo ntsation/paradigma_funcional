7a3cc574e250e39f0574968518470b7bcebd3a36
Nathan Souza <naathanmarinho11@gmail.com>
2024-06-15 10:53:07 -0300

    adiciona melhorias no codigo

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 from functools import reduce
 
-# Função para calcular o quadrado de um número
+numeros = [1, 2, 3, 4, 5]
+
+listas_aninhadas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
 def quadrado(x):
     return x ** 2
 
-# Função para somar dois números
 def somar(x, y):
     return x + y
 
-# Lista de números
-numeros = [1, 2, 3, 4, 5]
+def e_impar(x):
+    return x % 2 != 0
 
-# Aplica a função quadrado a cada número na lista
-quadrados = map(quadrado, numeros)
+def exibir_informacoes(lista):
+    for i, num in enumerate(lista):
+        print(f"Número {i+1}: {num}")
 
-# Soma os quadrados resultantes
-soma_dos_quadrados = reduce(somar, quadrados)
+def achatar(lista):
+    return [item for sublista in lista for item in sublista]
 
-print("Soma dos quadrados:", soma_dos_quadrados)
+impares = list(filter(e_impar, numeros))
+quadrados_impares = list(map(quadrado, impares))
+print("Números ímpares:")
+exibir_informacoes(impares)
+
+
+print("\nQuadrados dos números ímpares:")
+exibir_informacoes(quadrados_impares)
+
+
+soma_dos_quadrados_impares = reduce(somar, quadrados_impares)
+print("\nSoma dos quadrados dos números ímpares:", soma_dos_quadrados_impares)
+
+
+lista_achatada = achatar(listas_aninhadas)
+print("\nLista aninhada achatada:", lista_achatada)
+
+
+soma_dos_quadrados_achatados = reduce(somar, map(quadrado, lista_achatada))
+print("Soma dos quadrados da lista aninhada achatada:", soma_dos_quadrados_achatados)

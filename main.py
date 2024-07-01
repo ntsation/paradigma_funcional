89faddb4c5a44ed97aeb538e1367b74f05fde13f
Nathan Souza <naathanmarinho11@gmail.com>
2024-07-01 14:59:42 -0300

    ajusta formatação do código

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ numeros = [1, 2, 3, 4, 5]
 
 listas_aninhadas = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+
 def quadrado(x):
     return x ** 2
+
 
 def somar(x, y):
     return x + y
 
+
 def e_impar(x):
     return x % 2 != 0
 
+
 def exibir_informacoes(lista):
     for i, num in enumerate(lista):
-        print(f"Número {i+1}: {num}")
+        print(f"Número {i + 1}: {num}")
+
 
 def achatar(lista):
     return [item for sublista in lista for item in sublista]
+
 
 impares = list(filter(e_impar, numeros))
 quadrados_impares = list(map(quadrado, impares))
@@ -39,4 +45,5 @@ print("\nLista aninhada achatada:", lista_achatada)
 
 
 soma_dos_quadrados_achatados = reduce(somar, map(quadrado, lista_achatada))
-print("Soma dos quadrados da lista aninhada achatada:", soma_dos_quadrados_achatados)
+print("Soma dos quadrados da lista aninhada achatada:",
+      soma_dos_quadrados_achatados)
